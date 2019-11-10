b4384dc79724ca70a05fce9f9a2dc2cdc672a0a0
sayeed958 <md.sayeed958@gmail.com>
2019-11-11 00:40:42 +0400

    unit test cases written

--- test/card.test.ts
+++ test/card.test.ts
@@ -5,33 +5,66 @@
 import * as chai from 'chai';
 import 'mocha';
 import * as request from 'request';
+import {copyFileSync} from "fs";
 
-const { expect } = chai;
-const serverUrl = 'http://localhost:3000/api/v1/user';
-const token =	'';
+const {expect} = chai;
+const serverUrl = 'http://localhost:4000/api/v1';
+const token = '';
 
-describe('user test API', () => {
-	before((done) => {
-		console.log('before test case');
-		done();
-	});
+describe('Credit card test API', () => {
+    before((done) => {
+        done();
+    });
 
-	describe('Get User Profile', () => {
-		it('Should Get User Profile Data', (done) => {
-			const dataToSend = {
-				url: `${serverUrl}/profile`,
-				method: 'GET',
-				headers: {
-					Authorization: token
-				}
-			};
-			request(dataToSend, (err, res, body) => {
-				body = JSON.parse(body);
-				expect(err).to.be.null;
-				expect(body.status).to.equal(true);
-				expect(body).to.haveOwnProperty('result');
-				done();
-			});
-		});
-	});
+
+    describe('when the request has a missing item in payload', () => {
+        it('should return a 400 ok response and a single error', (done) => {
+
+            const payload = {
+                name: 'andrew',
+                limit: 10
+            };
+
+            const dataToSend = {
+                url: `${serverUrl}/card/add-card`,
+                method: 'POST',
+                json: payload,
+                headers: {}
+            };
+            request(dataToSend, (err, response, body) => {
+
+                expect(err).to.be.null;
+                expect(body.status).to.equal(400);
+                done();
+            })
+
+        });
+    });
+
+    describe('When sending invalid credit card number in payload', () => {
+        it('should return invalid credit card 400', (done) => {
+
+            const payload = {
+                name: 'andrew',
+                limit: 10,
+                cardNumber: 123123123123
+            };
+
+            const dataToSend = {
+                url: `${serverUrl}/card/add-card`,
+                method: 'POST',
+                json: payload,
+                headers: {}
+            };
+            request(dataToSend, (err, response, body) => {
+                expect(err).to.be.null;
+                expect(body.status).to.equal(false);
+                expect(body.code).to.equal(400);
+                done();
+            })
+
+        });
+    });
+
+
 });
